01b19a4f8e5818f3ea3f948d1fa4742284ec3f8b
Taran Cacacho <tarancacacho@gmail.com>
2019-07-03 22:36:16 -0700

    Remove name and email columns, add username column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,21 @@ class UsersController < ApplicationController
     render json: User.all
   end
 
-  def create
-    user = User.new(user_params)
-    if user.save
-      render json: user
-    else
-      render json: user.errors.full_messages, status: :unprocessable_entity
-    end
-  end
-
   def show
     user = User.find(params[:id])
     if user
       render json: user
     else
       render json: user.errors.full_messages, status: :not_found
+    end
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id]) 
     if user.destroy
-      render plain: "User destroyed"
+      render json: user
     else
       render json: user.errors.full_mnessages, status: :unprocessable_entity
     end
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :age)
+    params.require(:user).permit(:username)
   end
 end
