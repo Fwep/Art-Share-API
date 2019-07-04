57152905c799c87dc350542a7ef33ff23a441620
Taran Cacacho <tarancacacho@gmail.com>
2019-07-03 21:20:25 -0700

    Construct data layer

---
+++ app/models/artwork_share.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: artwork_shares
+#
+#  id         :bigint           not null, primary key
+#  artwork_id :integer          not null
+#  viewer_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class ArtworkShare < ApplicationRecord
+  validates :artwork_id, uniqueness: {scope: :viewer_id}
+  validates :artwork_id, :viewer_id, presence: true
+
+  belongs_to :artwork
+
+  belongs_to :viewer,
+    class_name: 'User'
+end
---
+++ db/migrate/20190704023507_add_and_drop_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddAndDropColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :name, :string
+    remove_column :users, :age, :integer
+    add_column :users, :username, :string, null: false
+      add_index :users, :username, unique: true
+  end
+end
