a4bfa9ef2b2e73b5f6c65bf101ce21791ec3f346
Taran Cacacho <tarancacacho@gmail.com>
2019-07-05 18:15:37 -0700

    Add CRD actions for comments resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :users do 
     resources :artworks, only: :index
   end
+  
   resources :artworks, only: [:create, :show, :update, :destroy]
+
   resources :artwork_shares, only: [:create, :destroy]
+
+  resources :comments, only: [:create, :destroy, :index]
 end
