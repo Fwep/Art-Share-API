644977a4d3c0c2aada0cc45ea3ba10f5affee65a
Taran Cacacho <tarancacacho@gmail.com>
2019-07-05 18:15:04 -0700

    Add  column to  table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_000640) do
+ActiveRecord::Schema.define(version: 2019_07_06_003753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_07_06_000640) do
     t.integer "artwork_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
     t.index ["artwork_id"], name: "index_comments_on_artwork_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
   end
